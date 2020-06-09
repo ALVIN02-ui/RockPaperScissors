db4e6fa55eb08ff5aecea1e45fb6e1e7c25e878f
ALVIN02-ui <57347701+ALVIN02-ui@users.noreply.github.com>
2020-06-09 10:19:27 +0300

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'ALVIN';
+  title = 'ALVIN SAYS HI RENATA';
   userSelected:string
 computerSelected:string
 computerweapon=["rock","paper","scissors"]
